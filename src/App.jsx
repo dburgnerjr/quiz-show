49c8c95972e7574ceb9514ef3824f013aab8eaa3
Daniel Burgner <dburgnerjr@yahoo.com>
2025-10-08 01:11:29 -0500

    Added quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
+import Quiz from './components/quiz'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +10,10 @@ function App() {
   return (
     <div className="app-container">
       <h1>Quiz Show</h1>
+
+      <Quiz />
     </div>
   )
 }
 
-export default App
+export default App;
